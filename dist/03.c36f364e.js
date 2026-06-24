5dfe72b72ba0a887dd80a3ad37b8c139fcd1f87c
Vanshdev21 <vanshkhubchandani94@gmail.com>
2026-06-24 19:15:12 +0530

    commit 2

---
+++ dist/03.c36f364e.js
@@ -0,0 +1,9 @@
+let container = React.createElement('div', {
+    id: "parent"
+}, React.createElement('div', {
+    id: "Child "
+}, React.createElement('h1', [], "Hello React")));
+let root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(container);
+
+//# sourceMappingURL=03.c36f364e.js.map
