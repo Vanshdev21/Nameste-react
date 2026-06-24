2ada89af5e1796f4a36c66eac9b09314972fb5b7
Vanshdev21 <vanshkhubchandani94@gmail.com>
2026-06-25 02:08:24 +0530

    practice done

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 import React from "react";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-let container = React.createElement(
-    'div',
-    {id: "parent"},
-    React.createElement(
-        'div',
-        {id: "Child "},
-        React.createElement('h1',[],"Hello React")
-    )
-)
+// React Element ==> Object ==> HTML Element
 
-let root = createRoot(document.querySelector("#root"));
+// const heading = React.createElement("h1",{id: "heading"},"Hellooo Vansh")
 
-root.render(container);
+// JSX
+// React ELement
+const jsxHeading = <h1>Hello World - VK</h1>;
+
+// React Component
+//1. Class based component - OLD
+//2. Functional based component - NEW => Just a normal js function
+
+const Heading = () => {
+  return (
+    <div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTktPU5elnGJYdqJqIwX6UdNGpT63ziwOUF45F-Wa3E0mA85wy68utTksAU&s=10"
+        height="100"
+        width="100"
+      />
+      {jsxHeading}
+      <p>Age: 18</p>
+    </div>
+  );
+};
+
+const root = createRoot(document.querySelector("#root"));
+
+root.render(<Heading />);
